2b2d0cd8b161fbf14e666a4893c4f2e6eeee4dc1
SIVARAMAKRISHNA <51481385+ullagallu123@users.noreply.github.com>
2025-03-28 09:25:43 +0530

    update the cluster

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# eks-cluster
+# eks-cluster creation using aws cli
+- create iam user with admin privileges
+- install aws cli
+- install eksctl
+- install kubectl compatible with eks
+- run the script cluster.sh to create control plane managed by aws
+- run the script node.sh create to create node group
+- run the script delete.sh to delete the all the resources 
---
+++ eks-cluster/node.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+# Variables
+CLUSTER_NAME="ullagallu-konkas-tech"
+REGION="ap-south-1"
+NODEGROUP_NAME="ng1"
+NODE_TYPE="t3a.medium"
+NODES=1
+NODES_MIN=1
+NODES_MAX=1
+NODE_VOLUME_SIZE=20
+SSH_PUBLIC_KEY="siva"
+PROFILE="eks-siva.bapatlas.site"
+LOG_FILE="/tmp/eks_nodegroup_$(date +%Y-%m-%d_%H-%M-%S).log"
+
+# Function to calculate elapsed time
+elapsed_time() {
+    local start=$1
+    local end=$2
+    local elapsed=$(( end - start ))
+    local minutes=$(( elapsed / 60 ))
+    local seconds=$(( elapsed % 60 ))
+    printf "Elapsed time: %d minutes and %d seconds\n" $minutes $seconds | tee -a "$LOG_FILE"
+}
+
+# Start timer
+start_time=$(date +%s)
+
+# Create nodegroup
+echo "Creating nodegroup..." | tee -a "$LOG_FILE"
+eksctl create nodegroup --cluster=$CLUSTER_NAME \
+                       --region=$REGION \
+                       --name=$NODEGROUP_NAME \
+                       --node-type=$NODE_TYPE \
+                       --nodes=$NODES \
+                       --nodes-min=$NODES_MIN \
+                       --nodes-max=$NODES_MAX \
+                       --node-volume-size=$NODE_VOLUME_SIZE \
+                       --ssh-access \
+                       --ssh-public-key=$SSH_PUBLIC_KEY \
+                       --profile $PROFILE \
+                       --managed \
+                       --asg-access \
+                       --external-dns-access \
+                       --spot  &>>"$LOG_FILE"
+if [ $? -ne 0 ]; then
+    echo "Error: Failed to create nodegroup." | tee -a "$LOG_FILE"
+    exit 1
+fi
+
+# End timer
+end_time=$(date +%s)
+
+# Calculate and print elapsed time
+elapsed_time $start_time $end_time
+
+
+# --spot  &>>"$LOG_FILE"
